19ac52c675f81b3d864e538e17fe14814463f727
Nailton Oliveira <nailton.me91@gmail.com>
2025-09-10 23:59:56 -0300

    chore: add husky, lintstaged and commitlint to ensure commit pattern

---
+++ .lintstagedrc.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [
+    buildEslintCommand,
+    // buildVitestCommand,
+  ],
+}

91306d5ed1d7e451c6c8577832055a89a48f9d95
Nailton Oliveira <nailton.me91@gmail.com>
2025-09-11 00:14:23 -0300

    chore: setup vitest to run unit tests

---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./setupTests.ts", "./vitest.setup.ts"],
+    coverage: {
+      provider: "v8",
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        "**/{next,postcss,tailwind}.config.?(c|m)[jt]s",
+        "./src/test-helpers",
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
+});
---
+++ vitest.setup.ts
@@ -0,0 +1,19 @@
+import { vi } from "vitest";
+import { loadEnvConfig } from "@next/env";
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
+
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
